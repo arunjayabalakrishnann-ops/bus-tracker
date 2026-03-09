b2644f604d81705df0ab68c1d62ca4d575c55085
Arun Jayabalakrishna <arunjayabalakrishnan@outlook.com>
2026-03-09 10:29:32 +0530

    move project to src

--- src/server.js
+++ src/server.js
@@ -3,15 +3,12 @@ const path = require("path");
 
 const app = express();
 
-// serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
-// homepage
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// driver page
 app.get("/driver", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "driver.html"));
 });

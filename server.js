653e72ec2689e2bf5a77e06ca0eebdf2261a21ca
Arun Jayabalakrishna <arunjayabalakrishnan@outlook.com>
2026-03-09 10:10:54 +0530

    final fix

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
 const express = require("express");
 const path = require("path");
+
 const app = express();
 
-// Serve static files from the public folder
-app.use(express.static(path.join(__dirname, "./public")));
+// serve static files
+app.use(express.static(path.join(__dirname, "public")));
 
-// Homepage route
+// homepage
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// Driver panel route
+// driver page
 app.get("/driver", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/driver.html"));
+  res.sendFile(path.join(__dirname, "public", "driver.html"));
 });
 
-// Optional: other routes if you add more HTML pages
-// app.get("/commuter", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./public/commuter.html"));
-// });
-
-// Start the server
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on port " + PORT);
 });

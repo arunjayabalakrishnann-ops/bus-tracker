6a9ae2e3607000c63f57619d4e3104960b4c6aba
Arun Jayabalakrishna <arunjayabalakrishnan@outlook.com>
2026-03-11 19:06:49 +0530

    final bus icon version

--- src/public/script.js
+++ src/public/script.js
@@ -1,74 +1,99 @@
 const socket = io();
 
-// Create map
+// create map
 const map = L.map('map').setView([11.0168,76.9558],18);
 
-// Map tiles
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-    maxZoom:19
+maxZoom:19
 }).addTo(map);
 
 
 // BUS ICON
 const busIcon = L.icon({
-    iconUrl: "bus.png",
-    iconSize: [50,50],
-    iconAnchor: [25,25]
+iconUrl: "bus.png",
+iconSize: [45,45],
+iconAnchor: [22,22]
 });
 
 
 let buses = {};
-let userMarker = null;
+let userMarker;
 
 
-// Receive bus GPS
+// distance function
+function getDistance(lat1,lon1,lat2,lon2){
+
+const R = 6371;
+
+const dLat = (lat2-lat1)*Math.PI/180;
+const dLon = (lon2-lon1)*Math.PI/180;
+
+const a =
+Math.sin(dLat/2)*Math.sin(dLat/2) +
+Math.cos(lat1*Math.PI/180) *
+Math.cos(lat2*Math.PI/180) *
+Math.sin(dLon/2)*Math.sin(dLon/2);
+
+const c = 2*Math.atan2(Math.sqrt(a),Math.sqrt(1-a));
+
+return R*c;
+
+}
+
+
+// receive bus gps
 socket.on("busLocation",(data)=>{
 
-    const id = data.busId;
-    const lat = data.lat;
-    const lng = data.lng;
-    const crowd = data.crowd;
+const id = data.busId;
+const lat = data.lat;
+const lng = data.lng;
+const crowd = data.crowd;
 
-    // If bus not created
-    if(!buses[id]){
 
-        buses[id] = L.marker([lat,lng],{
-            icon: busIcon
-        }).addTo(map);
+// create bus marker
+if(!buses[id]){
 
-    }
+buses[id] = L.marker([lat,lng],{
+icon: busIcon
+}).addTo(map);
 
-    // Move bus
-    buses[id].setLatLng([lat,lng]);
+}
 
-    // Popup info
-    buses[id].bindPopup(
-        "<b>"+id+"</b><br>"+
-        "Crowd: "+crowd
-    );
+
+// move bus
+buses[id].setLatLng([lat,lng]);
+
+
+// popup details
+buses[id].bindPopup(
+
+"<b>"+id+"</b><br>"+
+"Crowd Level: "+crowd
+
+);
 
 });
 
 
-// User location
+// commuter location
 navigator.geolocation.watchPosition((pos)=>{
 
-    const lat = pos.coords.latitude;
-    const lng = pos.coords.longitude;
+const lat = pos.coords.latitude;
+const lng = pos.coords.longitude;
 
-    if(!userMarker){
+if(!userMarker){
 
-        userMarker = L.marker([lat,lng]).addTo(map)
-        .bindPopup("Your Location");
+userMarker = L.marker([lat,lng]).addTo(map)
+.bindPopup("Your Location");
 
-        map.setView([lat,lng],18);
+map.setView([lat,lng],18);
 
-    } else {
+}else{
 
-        userMarker.setLatLng([lat,lng]);
+userMarker.setLatLng([lat,lng]);
 
-    }
+}
 
 },{
-    enableHighAccuracy:true
+enableHighAccuracy:true
 });
